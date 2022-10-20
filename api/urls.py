3315f13971bcebc7a2ee303fcd1735414bfbb0c0
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-10-20 09:01:50 +0200

    chore(URLConfig): normalize name convintions

--- api/urls.py
+++ api/urls.py
@@ -7,17 +7,16 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="URI - Backend API",
+        title="Tierra - Backend API",
         default_version="v1",
-        description="Underlying api for URI website",
+        description="Underlying api for Tierra E-Learning Platform",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="yusufadell.dev@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
 )
-
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("signup/", TemplateView.as_view(template_name="signup.html"), name="signup"),
@@ -70,14 +69,17 @@ urlpatterns = [
         name="profile-redirect",
     ),
     # Courses API
-    path("api/v1/", include("courses.api.urls", namespace="courses_api")),
+    path(
+        "api/v1/",
+        include("courses.api.urls", namespace="courses-api"),
+    ),
     # swagger urls
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="api_docs"),
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="api-docs"),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
