7b73fdc4ead1b1cee04a74bd50d06ef7a0a37d8d
yusufadell <yusufadell.dev@gmail.com>
2023-01-23 14:50:30 +0200

    added pagination

--- api/settings.py
+++ api/settings.py
@@ -125,6 +125,8 @@ REST_FRAMEWORK = {
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15,
 }
 
 SWAGGER_SETTINGS = {
